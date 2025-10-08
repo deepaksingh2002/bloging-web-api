c2d57e7cf246720a157d9423e91a77d399c40bc6
Deepak Singh <deepakksingh1202@gmail.com>
2025-10-08 23:00:56 +0530

    Add to DB Connection.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import {app} from "./src/app.js";
 import {config} from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
 
-const port = config.port || 8000;
-app.listen(port, () => console.log(`Server is running on: ${port}`))
+
+connectDB()
+.then(() =>{
+    const port = config.port || 8000;
+    app.listen(port, () => console.log(`Server is running on: ${port}`));
+    })
+.catch((error)=>{
+    console.log("mongoDB connection faild !!!", error);
+})
---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {config} from "../config/config.js"
+import {DB_NAME} from "../../constants.JS";
+
+const connectDB = async() => {
+    try {
+        const connectInstance= await mongoose.connect(`${config.databaseUrl}/${DB_NAME}`)
+        console.log(`MongoDB connected!! DB_HOST: ${connectInstance.connection.host}`);
+    } catch (error) {
+        console.log("Mongose connection error: ", error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB
