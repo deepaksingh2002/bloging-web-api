55b5e32bf511527855234f78cc309fca43426b05
Deepak Singh <deepakksingh1202@gmail.com>
2025-10-16 23:02:23 +0530

    end point initel setup

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import usoerRouter from "./router/user.router.js"
+
+app.use("api/v1/user", usoerRouter)
+
 
 
 export {app}
---
+++ src/router/user.router.js
@@ -0,0 +1,11 @@
+import Router, { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import {} from "../controllers/user.controllers.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+
+
+
+export default router
