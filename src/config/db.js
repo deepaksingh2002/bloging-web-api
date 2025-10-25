e9f9cedde32bab4ca0e46e6e338bf733d8ed9492
Deepak Singh <deepakksingh1202@gmail.com>
2025-10-25 11:13:40 +0530

    fix syntex error Js to js extension

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import {config} from "../config/config.js";
-import {DB_NAME} from "../../constants.JS";
+import {DB_NAME} from "../../constants.js";
 
 const connectDB = async() => {
     try {
