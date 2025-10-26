2837727340362c302bb562033aa9cab7814557cd
Deepak Singh <deepakksingh1202@gmail.com>
2025-10-26 23:56:05 +0530

    Add login endPoint.

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import {registerUser} from "../controllers/user.controllers.js"
+import {LogInUser, registerUser} from "../controllers/user.controllers.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,9 +8,9 @@ const router = Router();
 router.route("/register").post(
     upload.fields({name: "avatar", maxCount: 1}),
     registerUser
-)
+);
+router.route("/login").post(LogInUser);
 
-router.route
 
 
 export default router
