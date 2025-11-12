47dd6b12c1841db4ff7b18a545d9219b37c7e3b2
Deepak Singh <157193532+deepaksingh2002@users.noreply.github.com>
2025-11-12 20:19:36 +0000

    Add like functionality for posts and comments, including toggle and retrieval endpoints

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,78 @@
+import asyncHandler from "../helpers/asyncHandler.js";
+import ApiError from "../helpers/ApiError.js";
+import ApiResponse from "../helpers/ApiResponse.js";
+import Post from "../models/post.model.js";
+import Like from "../models/like.model.js";
+
+const togglePostLike = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    const existingLike = await Like.findOne({ post: postId, user: userId });
+
+    if (existingLike) {
+        // If like exists, remove it (unlike)
+        await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(
+            new ApiResponse(200, null, "Post unliked successfully")
+        );
+    } else {
+        // If like does not exist, create it (like)
+        const newLike = new Like({
+            post: postId,
+            user: userId
+        });
+        await newLike.save();
+        return res.status(200).json(
+            new ApiResponse(200, null, "Post liked successfully")
+        );
+    }
+});
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    // Implementation for toggling comment like
+    const { commentId } = req.params;
+    const userId = req.user._id;
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    const existingLike = await Like.findOne({ comment: commentId, user: userId });
+
+    if (existingLike) {
+        // If like exists, remove it (unlike)
+        await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(
+            new ApiResponse(200, null, "Comment unliked successfully")
+        );
+    } else {
+        // If like does not exist, create it (like)
+        const newLike = new Like({
+            comment: commentId,
+            user: userId
+        });
+        await newLike.save();
+        return res.status(200).json(
+            new ApiResponse(200, null, "Comment liked successfully")
+        );
+    }   
+});
+
+const getLikedPosts = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+
+    const likedPosts = await Like.find({ user: userId }).populate('post');
+
+    return res.status(200).json(
+        new ApiResponse(200, likedPosts.map(like => like.post), "Liked posts fetched successfully")
+    );
+});
+
+export { togglePostLike, toggleCommentLike, getLikedPosts };
