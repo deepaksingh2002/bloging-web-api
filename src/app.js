0b4b88689b22452b97010f55124c19854c559915
Deepak Singh <157193532+deepaksingh2002@users.noreply.github.com>
2025-12-12 20:28:20 +0000

    update cors

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 app.use(express.json({ limit: "16kb" }));
