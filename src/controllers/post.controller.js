914085a80def2a733c4bc27b0a65db0ef8f66240
Deepak Singh <deepakksingh1202@gmail.com>
2026-02-27 02:31:58 +0530

    add owner avatar in post response.

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -76,7 +76,7 @@ const createPost = asyncHandler(async (req, res) => {
 const getPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find()
     .select("title thumbnail owner createdAt views")
-    .populate("owner", "username")
+    .populate("owner", "username", "avatar")
     .sort({ createdAt: -1 })
     .lean();
   if (!posts.length) {
