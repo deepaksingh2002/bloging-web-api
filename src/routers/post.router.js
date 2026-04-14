e91f7ef125d8875e173b0a010487f02c0793d6d5
Deepak Singh <deepakksingh1202@gmail.com>
2026-04-14 15:36:03 +0530

    feat(router): expose post report route

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -9,6 +9,7 @@ import {
   getPostById,
   deletePost,
   updatePost,
+  reportPost,
 } from "../controllers/post.controller.js";
 import { verifyJWT, verifyJWTOptional } from "../middlewares/auth.middleware.js";
 import { requireAuthor } from "../middlewares/role.middleware.js";
@@ -21,6 +22,7 @@ router.route("/getAll-post").get(verifyJWTOptional, getPosts);
 router.route("/search-post").get(verifyJWTOptional, searchPosts);
 
 router.route("/get-post/:postId").get(verifyJWTOptional, getPostById);
+router.route("/:postId/report").post(verifyJWT, reportPost);
 
 router.route("/delete-post/:postId").delete(verifyJWT, requireAuthor, deletePost);
 
